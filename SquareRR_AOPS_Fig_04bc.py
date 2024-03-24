2f5bb46449e678b7fb3b9db39d7c1c0e59633dee
E. Adibnia <ehsan.adibnia@gmail.com>
2024-03-24 21:38:24 +0330

    SquareRR_AOPS_Fig_04bc.py

--- SquareRR_AOPS_Fig_04bc.py
+++ SquareRR_AOPS_Fig_04bc.py
@@ -1,5 +1,5 @@
 """
-The code for the research presented in the paper titled "Spectrum prediction and inverse design of all-optical nonlinear plasmonic square ring resonator switches using deep learning
+The code for the research presented in the paper titled "Enabling inverse design of plasmon-based nonlinear square resonators with deep neural networks."
 
 This code corresponds to the article's Forward Deep Neural Network (DNN) section.
 This code regenerates the Fig. 4b and c of the paper.
